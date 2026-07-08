8cdedd29472b1d50d334bd364160f65b8267e303
tiagosegato <tiago.segato@ifb.edu.br>
2026-07-08 16:32:07 -0300

    add condicionais e repetição

---
+++ logica-js/5-condicionais.js
@@ -0,0 +1,92 @@
+//IF Simples
+
+/*
+let velocidade = 120
+
+if (velocidade > 80) {
+    console.log("Você foi multado!")
+}
+
+console.log("Siga com segurança!")
+*/
+
+//IF / ELSE
+/*
+let idade = 12
+
+if (idade >= 18) {
+    console.log("É maior de idade!")
+} else {
+    console.log("É menor de idade!")
+}
+
+console.log("Aqui sempre é executado...")
+*/
+
+//IF / ELSE IF
+/*
+let idade = 70
+
+if (idade < 12) {
+    console.log("Entrada Proibida!")
+} else if (idade >= 12 && idade <= 18) {
+    console.log("Entrada Permitida com responsável!")
+    let
+} else {
+    console.log("Entrada Permitida!")
+}
+*/
+
+//TERNÁRIO
+/*
+//Exemplo com IF/ELSE
+let resultado
+
+if (num % 2 == 0) {
+    resultado = "Par";
+} else {
+    resultado = "Ímpar";
+}
+
+console.log(resultado);
+
+//Equivalente com TERNÁRIO
+let num = 2;
+
+let resultado = (num % 2 == 0) ? "Par" : "Ímpar";
+
+console.log(resultado);
+*/
+
+//SWITCH
+/*
+let dia = 12
+
+switch (dia) {
+    case 1:
+        console.log('Domingo')
+        break
+    case 2:
+        console.log('Segunda')
+        break
+    case 3:
+        console.log('Terça')
+        break
+    case 4:
+        console.log('Quarta')
+        break
+    case 5:
+        console.log('Quinta')
+        break
+    case 6:
+        console.log('Sexta')
+        break
+    case 7:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Dia inválido!')
+}
+*/
+
+
---
+++ logica-js/6-repeticao.js
@@ -0,0 +1,55 @@
+//FOR
+/*
+console.log("Iniciando...")
+
+for (let i = 0; i < 5; i++) {
+    console.log(i) //executa 5 vezes (mostra de 0 a 4)
+}
+
+console.log('Fora do Loop...')
+*/
+
+//FOR para ARRAYS
+/*
+const carros = ["Ford", "Chevrolet", "Fiat"];
+
+//For Tradicional:
+for (let i = 0; i < carros.length; i++) {
+    console.log(carros[i]);
+}
+
+//For IN
+for (let i in carros) {
+    console.log(carros[i]);//usa o índice
+}
+
+//For OF
+for (let carro of carros) {
+    console.log(carro);//usa o valor
+}
+
+//For EACH
+carros.forEach(function (value, index) {
+    console.log(index, value);//usa indice e valor para executar uma ação
+})
+*/
+
+//WHILE
+/*
+let i = 1
+
+while (i <= 5) {
+    console.log(`Loop rodando... ${i} vezes.`)
+    i++
+}
+*/
+
+//DO WHILE
+/*
+let i = 0
+
+do {
+    console.log(i)
+    i++
+} while (i < 5)
+*/
