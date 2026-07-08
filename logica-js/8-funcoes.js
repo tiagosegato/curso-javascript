d3afa2bdd504d1e614727ac54435aa8ba2809f2f
tiagosegato <tiago.segato@ifb.edu.br>
2026-07-08 20:04:08 -0300

    add functions

---
+++ logica-js/8-funcoes.js
@@ -0,0 +1,46 @@
+//Função Básica
+/*
+//Declarando a função
+function basica() {
+    console.log('Olá, Mundo!')
+}
+
+//Chamando a função
+basica()
+*/
+
+
+//Função com parâmetros e retorno
+/*
+function soma(n1, n2) {
+    let resultado = n1 + n2
+
+    return resultado
+}
+
+console.log(soma(2, 5))
+*/
+
+//Funções em variáveis
+/*
+let resultadoSoma = function soma(n1, n2) {
+    return n1 + n2
+}
+
+console.log(resultadoSoma(3, 6))
+*/
+
+//Arrow Functions
+/*
+//Com corpo e return
+let soma3 = (n1, n2) => {
+    return n1 + n2
+}
+
+console.log(soma3(8, 3))
+
+//Mais curta e com retorno implícito
+let soma4 = (n1, n2) => n1 + n2
+
+console.log(soma4(9, 4))
+*/
